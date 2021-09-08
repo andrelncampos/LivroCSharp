ce93dd5f5afabd881b53ae2ef52a30559c3d36b8
Sistema Moderno <41524488+andrelncampos@users.noreply.github.com>
2021-09-07 22:58:43 -0300

    .

--- LivroCSharp/MinhasClasses/Produto.cs
+++ LivroCSharp/MinhasClasses/Produto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace LivroCSharp.MinhasClasses
+﻿namespace LivroCSharp.MinhasClasses
 {
 	public class Produto
 	{
