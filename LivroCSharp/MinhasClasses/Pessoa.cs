8a2983f5e22ed2eea96c53932c2e4c2cb8c00d2b
Sistema Moderno <41524488+andrelncampos@users.noreply.github.com>
2021-09-11 14:06:56 -0300

    .

---
+++ LivroCSharp/MinhasClasses/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace LivroCSharp.MinhasClasses
+{
+	public static class Pessoa
+	{
+		public static string Nome { get; set; }
+		public static string Endereco { get; set; }
+		public static int Idade { get; set; }
+	}
+}
