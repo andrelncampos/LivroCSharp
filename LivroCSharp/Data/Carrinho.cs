013fae2d8c7a945cf6930954d1154dbc79f2b7dc
Sistema Moderno <41524488+andrelncampos@users.noreply.github.com>
2021-09-18 18:59:49 -0300

    .

---
+++ LivroCSharp/Data/Carrinho.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LivroCSharp.MinhasClasses;
+
+namespace LivroCSharp.Data
+{
+	public static class Carrinho
+	{
+
+		private static List<Item> Itens { get; set; } = new List<Item>();
+
+
+		public static List<Item> CarrinhoLista()
+		{
+			return Itens;
+		}
+
+		public static void CarrinhoAdicionar(Produto produto, int quantidade)
+		{
+			if (quantidade == 0) { return; }
+
+			Item item = Itens.Find(p => p.Produto.Codigo == produto.Codigo);
+
+			if (item == null)
+			{
+				// O item não existia no carrinho.
+				Itens.Add(new Item { Produto = produto, Quantidade = quantidade });
+			}
+			else
+			{
+				// O item já existia. Adicionar a quantidade.
+				item.Quantidade += quantidade;
+			}
+		}
+
+		public static void CarrinhoExcluir(Produto produto)
+		{
+			Item item = Itens.Find(p => p.Produto.Codigo == produto.Codigo);
+
+			if (item!=null)
+			{
+				Itens.Remove(item);
+			}
+		}
+
+		public static decimal CarrinhoTotal()
+		{
+			decimal retorno=0;
+
+			foreach(Item item in Itens)
+			{
+				retorno += (item.Produto.Preco * item.Quantidade);
+			}
+
+			return retorno;
+		}
+	}
+}
---
+++ LivroCSharp/MinhasClasses/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LivroCSharp.MinhasClasses
+{
+	public class Item
+	{
+		public Produto Produto { get; set; }
+		public int Quantidade { get; set; }
+		public double Desconto { get; set; }
+	}
+}
